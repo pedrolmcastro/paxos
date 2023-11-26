12890740529de158986aa19fb14307985c6a9801
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2023-11-26 16:10:09 -0300

    [feat] calculate the majority for the paxos algorithm

--- src/server.py
+++ src/server.py
@@ -5,17 +5,24 @@ import host
 
 
 def main():
+    # Parse CLI inputs
     parser = cli.Parser.server
     parsed = parser.parse_args(sys.argv[1:])
 
+    # Parse hostfile
     try:
         hosts = host.from_hostfile(parsed.hostfile)
     except Exception as exception:
         parser.error(str(exception))
 
+    # Calculate the majority for the Paxos algorithm
+    majority = len(hosts) // 2 + 1
+
+
     print(f"Port: {parsed.port}")
     print(f"Hostfile: {parsed.hostfile}")
     print(f"Hosts: {' '.join(map(str, hosts))}")
+    print(f"Majority: {majority}")
 
 
 if __name__ == "__main__":
