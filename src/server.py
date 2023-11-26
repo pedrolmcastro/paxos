4e8f67f0e43b7c8182554020fc6c9a7d7ff1cc26
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2023-11-26 16:27:19 -0300

    [feat] generate uuid and log the progress

--- src/server.py
+++ src/server.py
@@ -1,29 +1,48 @@
 import sys
+import logging
+from uuid import UUID, uuid4
 
 import cli
-import host
+from port import Port
+from host import Host, from_hostfile
 
 
 def main():
+    uuid, port, hosts, majority = parse()
+
+
+def parse() -> (UUID, Port, list[Host], int):
     # Parse CLI inputs
     parser = cli.Parser.server
     parsed = parser.parse_args(sys.argv[1:])
 
+    logging.debug(f"Parsed hostfile: {parsed.hostfile}")
+    logging.debug(f"Parsed port: {parsed.port}")
+
+
     # Parse hostfile
     try:
-        hosts = host.from_hostfile(parsed.hostfile)
+        hosts = from_hostfile(parsed.hostfile)
     except Exception as exception:
         parser.error(str(exception))
 
+    logging.debug(f"Parsed host list: [{', '.join(map(str, hosts))}]")
+
+
     # Calculate the majority for the Paxos algorithm
     majority = len(hosts) // 2 + 1
+    logging.debug(f"Calculated majority: {majority}")
 
 
-    print(f"Port: {parsed.port}")
-    print(f"Hostfile: {parsed.hostfile}")
-    print(f"Hosts: {' '.join(map(str, hosts))}")
-    print(f"Majority: {majority}")
+    # Generate the UUID
+    uuid = uuid4()
+    logging.debug(f"Generated UUID: {uuid}")
+
+
+    logging.info("Concluded parsing phase")
+    return uuid, parsed.port, hosts, majority
 
 
 if __name__ == "__main__":
+    logging.root.level = logging.DEBUG
     main()
