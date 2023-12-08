9865b1a1042545bb720702200e39c600283ca622
Pedro Lucas de Moliner de Castro <pedro.lucas.moliner.castro@gmail.com>
2023-12-08 03:57:00 -0300

    [feat] create a dataclass for the server parse return type

--- src/server.py
+++ src/server.py
@@ -1,8 +1,9 @@
-import sys
 import uuid
 import typing
 import asyncio
 import logging
+import pathlib
+import dataclasses
 
 from cli import Parser
 from util import Error
@@ -11,16 +12,24 @@ from connections import Connections
 from host import Host, Hostfile, Port
 
 
+@dataclasses.dataclass(frozen = True)
+class Info:
+    port: Port
+    majority: int
+    uid: uuid.UUID
+    hosts: list[Host]
+    hostfile: pathlib.Path
+
+
 async def main():
     logging.root.level = logging.DEBUG
-    uid, port, hosts, majority = parse()
-
+    info = parse()
 
     # This must be defined early to be accessable from the closures
     connections: Connections | None = None
 
     async def on_disconnect(length: int):
-        if length < majority:
+        if length < info.majority:
             await typing.cast(Connections, connections).close()
             Error.exit("Lost connection to the majority of the servers")
 
@@ -30,57 +39,48 @@ async def main():
     ) -> None:
         pass
 
-
     # try:
     #     server = await asyncio.start_server(serve, "localhost", port)
     # except Exception as exception:
     #     error(f"Failed to start server: {exception}")
 
+    connections = await Connections.connect(info.hosts, [1, 2, 5], on_disconnect)
 
-    connections = await Connections.connect(hosts, [1, 2, 5], on_disconnect)
-
-    if len(connections) < majority:
+    if len(connections) < info.majority:
         await connections.close()
         Error.exit("Failed to connect to the majority of the servers")
 
-
     # await server.serve_forever()
-
 
     message = Message.Accepted("Hello, world!", 0)
     await connections.broadcast(Message.encode(message))
 
 
-def parse() -> tuple[uuid.UUID, Port, list[Host], int]:
-    # Parse CLI inputs
-    parsed = Parser().server.parse_args(sys.argv[1:])
+def parse() -> Info:
+    parsed = Parser().server.parse_args()
+    logging.debug(f"Selected port: {parsed.port}")
+    logging.debug(f"Hostfile path: {parsed.hostfile}")
 
-    logging.debug(f"Port: {parsed.port}")
-    logging.debug(f"Hostfile: {parsed.hostfile}")
-
-
-    # Parse hostfile
     try:
         hosts = Hostfile.parse(parsed.hostfile)
     except Exception as exception:
         Error.exit(str(exception))
 
-    logging.debug(f"Hosts list: [{', '.join(map(str, hosts))}]")
+    logging.debug(f"Hosts: [{', '.join(map(str, hosts))}]")
 
-
-    # Calculate the majority for the Paxos algorithm
     majority = len(hosts) // 2 + 1
-    logging.debug(f"Majority: {majority}")
+    logging.debug(f"Calculated majority: {majority}")
 
-
-    # Generate the UID
     uid = uuid.uuid4()
-    logging.debug(f"UID: {uid}")
-
+    logging.debug(f"Generated UID: {uid}")
 
     logging.info("Parsing successfully concluded")
-    return uid, parsed.port, hosts, majority
+
+    return Info(parsed.port, majority, uid, hosts, parsed.hostfile)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        logging.info("Server closed")
